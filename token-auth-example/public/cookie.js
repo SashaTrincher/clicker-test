db41417a2b94b3ad3f93fbc9ee57366dafcd3d8b
SashaTrincher <oofman@ukr.net>
2023-12-06 11:05:56 +0100

    Tokens fetch

--- token-auth-example/public/cookie.js
+++ token-auth-example/public/cookie.js
@@ -1,8 +1,37 @@
-window.onload = async function fetchAuthToken() {
+// Function to get the value of a cookie by name
+function getCookie(name) {
+    const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    return match ? match[2] : null;
+}
+
+// Set cookie for authentication token
+function setAuthTokenCookie(token) {
+    document.cookie = `authToken=${token}; SameSite=Strict; Secure`;
+}
+
+// Function to check and remove an element by ID
+function checkAndRemoveElement(elementId) {
+    const removed = JSON.parse(localStorage.getItem(`${elementId}Removed`)) || false;
+    if (removed) {
+        const element = document.querySelector(`#${elementId}`);
+        if (element) {
+            element.remove();
+        }
+    }
+}
+
+// Function to fetch or create an authentication token
+async function fetchAuthToken() {
     try {
         const authToken = getCookie('authToken');
         if (authToken) {
             console.log(`Existing user with this ${authToken} token.`);
+            // Check for the specific token and welcome the user
+            if (authToken === '56229b83-c7f7-467e-8e28-93a0ec01ed5c') {
+                console.log('Welcome Alex');
+                // Fetch all issued tokens from the server
+                fetchAllTokens();
+            }
             return authToken;
         } else {
             // If the cookie is not present, fetch a new token
@@ -18,21 +47,16 @@ window.onload = async function fetchAuthToken() {
     }
 }
 
-// Function to get the value of a cookie by name
-function getCookie(name) {
-    const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
-    return match ? match[2] : null;
-}
-
-// Set HTTP-only cookie for authentication token
-function setAuthTokenCookie(token) {
-    document.cookie = `authToken=${token}; HttpOnly; SameSite=Strict; Secure`;
-}
-
-function checkAndRemoveElement(elementId) {
-    const removed = JSON.parse(localStorage.getItem(`${elementId}Removed`)) || false;
-    if (removed) {
-        const element = document.querySelector(`.${elementId}`);
-        element.remove();
+// Function to fetch all issued tokens from the server
+async function fetchAllTokens() {
+    try {
+        const response = await fetch('http://localhost:3000/getAllTokens', { mode: 'cors' });
+        const data = await response.json();
+        console.log('All issued tokens:', data.tokens);
+    } catch (error) {
+        console.error('Error fetching all tokens:', error);
     }
 }
+
+// Assign the fetchAuthToken function to window.onload
+window.onload = fetchAuthToken;
--- token-auth-example/server.js
+++ token-auth-example/server.js
@@ -5,6 +5,9 @@ const uuid = require('uuid');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Array to store issued tokens
+let issuedTokens = [];
+
 // Middleware to allow CORS
 app.use(cors({ origin: 'http://127.0.0.1:5500' }));
 
@@ -17,7 +20,13 @@ app.use((req, res, next) => {
 
 app.get('/getAuthToken', (req, res) => {
     const authToken = uuid.v4();
+    issuedTokens.push(authToken); // Store the issued token
     res.json({ token: authToken });
+});
+
+// Endpoint to get all issued tokens
+app.get('/getAllTokens', (req, res) => {
+    res.json({ tokens: issuedTokens });
 });
 
 app.use(express.static('public'));
